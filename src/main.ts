952dd3fb887fbb513994ba17d0d102d1af214db7
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-10-09 13:57:49 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#display')!.innerHTML = `
+  
+`
+
+function display(){
+
+}
+
+function clear(){
+
+}
+
+function add(){
+
+}
+
+function subtract(){
+
+}
+
+function divide(){
+
+}
+
+function multiply(){
+
+}
